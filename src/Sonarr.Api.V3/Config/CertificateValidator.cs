38dd4d225d0dff979c3269c0d3d9d04517abbce1
Mark McDowall <mark@mcdowall.ca>
2025-09-27 15:42:38 -0700

    New: Support PEM format for SSL certificates
    Closes #8087

--- src/Sonarr.Api.V3/Config/CertificateValidator.cs
+++ src/Sonarr.Api.V3/Config/CertificateValidator.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography.X509Certificates;
 using FluentValidation;
 using FluentValidation.Validators;
 using NLog;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Instrumentation;
 
 namespace Sonarr.Api.V3.Config
@@ -17,7 +18,7 @@ namespace Sonarr.Api.V3.Config
 
     public class CertificateValidator : PropertyValidator
     {
-        protected override string GetDefaultMessageTemplate() => "Invalid SSL certificate file or password. {message}";
+        protected override string GetDefaultMessageTemplate() => "Invalid SSL certificate file or {passwordOrKey}. {message}";
 
         private static readonly Logger Logger = NzbDroneLogger.GetLogger(typeof(CertificateValidator));
 
@@ -33,16 +34,31 @@ namespace Sonarr.Api.V3.Config
                 return true;
             }
 
+            var certPath = resource.SslCertPath;
+            var keyPath = resource.SslKeyPath;
+            var certPassword = resource.SslCertPassword;
+            var isPem = certPath.GetPathExtension() == ".pem" || keyPath.IsNotNullOrWhiteSpace();
+
             try
             {
-                new X509Certificate2(resource.SslCertPath, resource.SslCertPassword, X509KeyStorageFlags.DefaultKeySet);
+                if (isPem)
+                {
+                    X509Certificate2.CreateFromPemFile(certPath, keyPath.IsNullOrWhiteSpace() ? null : keyPath);
+                }
+                else
+                {
+                    new X509Certificate2(certPath, certPassword, X509KeyStorageFlags.DefaultKeySet);
+                }
 
                 return true;
             }
             catch (CryptographicException ex)
             {
-                Logger.Debug(ex, "Invalid SSL certificate file or password. {0}", ex.Message);
+                var passwordOrKey = isPem ? "key" : "password";
 
+                Logger.Debug(ex, "Invalid SSL certificate file or {0}. {1}", passwordOrKey, ex.Message);
+
+                context.MessageFormatter.AppendArgument("passwordOrKey", passwordOrKey);
                 context.MessageFormatter.AppendArgument("message", ex.Message);
 
                 return false;
